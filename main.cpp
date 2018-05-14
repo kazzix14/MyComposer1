384042f5dc489dcda9cd08036b04a654632157ed
Kazuma <atugari27@gmail.com>
2018-05-15 01:44:29 +0900

    modify main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,82 @@
 #include<iostream>
 #include<fstream>
 
+#define DEBUG
+
 using namespace std;
 
-int main()
-{
-    ofstream outputfile("test.txt");
-    outputfile<<"test";
-    outputfile.close();
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int UINT;
+
+void writeWord(ofstream *, WORD);
+
+int main(){
+
+    ofstream ofile("test.mid", ios::binary | ios::out);
+
+    //
+    if(!ofile){
+    	cout << "Failed to load file" << endl;
+    	return 1;
+    }
+
+    //data = 0xE00101000100060000006468544d; //MThd
+
+    //--------------------header chunk--------------------
+
+    //chunk type "MThd"
+    writeWord(&ofile, 0x4d54);
+    writeWord(&ofile, 0x6864);
+
+    //length
+    writeWord(&ofile, 0x0000);
+    writeWord(&ofile, 0x0006);
+
+    //format "01" (multi track)
+    writeWord(&ofile, 0x0001);
+
+    //number of tracks
+    writeWord(&ofile, 0x0001);
+
+    //division 2B 0x03c0...960
+    writeWord(&ofile, 0x03c0);
+
+    //--------------------track chunk--------------------
+
+    //chunk type "MTrk" 
+    writeWord(&ofile, 0x4d54);
+    writeWord(&ofile, 0x726b);
+
+    //length
+    writeWord(&ofile, 0x0000);/////////////////////////////////////
+    writeWord(&ofile, 0x0000);
+
+    //----------body----------
+
+    //delta time
+    writeWord(&ofile, 0x03c0);
+
+    //event
+    writeWord(&ofile, 0x8n00);
+
+    ofile.close();
+}
+
+void writeWord(ofstream *ofile, WORD datal){
+	WORD datab;
+
+	//datal = 0x4d54;
+
+    //リトルエンディアンからビッグエンディアン
+    datab = datal << 8;
+    datab |= datal >> 8;
+
+    #ifdef DEBUG
+    cout << datal << " " << endl;
+    #endif
+
+    ofile->write((char *) &datab, sizeof(datab));
+
+	return;
 }
